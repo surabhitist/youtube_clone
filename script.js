3f99934ebc19de297453f01ba6a3b9292d80947b
Surabhi <surabhikj@gmail.com>
2025-03-17 21:32:54 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function onhover() {
+  document.getElementById("list_item").style.backgroundColor =
+    "rgb(12, 12, 12)";
+}
+
+function hoverout() {
+  document.getElementById("list_item").style.backgroundColor =
+    "rgb(54, 54, 54)";
+}
